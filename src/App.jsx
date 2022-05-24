f6ec2641529657eb58d0e419bd2a9ba52ffc6ce3
gligor99 <lukagligorevic@gmail.com>
2022-05-24 20:55:22 +0200

    some examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Radi li</div>;
+  const [move, setMove] = useState(false);
+
+  return (
+    <div className="App">
+      {/* <motion.div
+        animate={{ x: move ? 360 : -220 }}
+        transition={{ type: "inertia" }}
+        onClick={() => setMove(!move)}
+        className="box"
+      ></motion.div> */}
+      {/* <motion.div
+        drag="x"
+        dragConstraints={{ left: 150 }}
+        className="box"
+      ></motion.div> */}
+      <motion.div
+        className="box"
+        animate={{ rotate: [0, 200, 200, 0], x: [0, 150, -150, 0] }}
+        transition={{ repeat: Infinity, duration: 1 }}
+      ></motion.div>
+    </div>
+  );
 }
 
 export default App;
